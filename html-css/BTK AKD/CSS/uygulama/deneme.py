193470582308e89c1ed5f0188b450fc5a3e1c089
EnesUnlu <enes9103@gmail.com>
2021-12-10 23:52:50 +0300

    güncelleme

---
+++ html-css/BTK AKD/CSS/uygulama/deneme.py
@@ -0,0 +1,28 @@
+# def printMax(a, b):
+#     if a > b:
+#         print(a, 'is maximum')
+#     elif a == b:
+#         print(a, 'is equal to', b)
+#     else:
+#         print(b, 'is maximum')
+# printMax(3, 4)
+
+
+# x = 50
+# def func(x):
+#     print('x is', x)
+#     x = 2
+#     print('Changed local x to', x)
+
+# func(x)
+# print('x is now', x)
+
+
+# def function1(var1=5, var2=7):
+#     var2=9
+#     var1=3
+#     print (var1, " ", var2)
+# function1(10,12)
+
+
+
