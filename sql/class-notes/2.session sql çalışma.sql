72839752fc873e53a89047696f0d1dd4656370a5
EnesUnlu <enes9103@gmail.com>
2021-12-16 00:25:20 +0300

    gün sonu güncelleme

--- sql/class-notes/2.session sql çalışma.sql
+++ sql/class-notes/2.session sql çalışma.sql
@@ -103,6 +103,20 @@
 	FROM invoices
 	WHERE InvoiceDate LIKE '201_-02-%';
 	
+	/* customers tablosunda, isimleri (FirstName) üüç harfli olan müşterilerin FirstName,
+	LastName, City ve Adress bilgilerini listeleyen sorguyu yazınız*/
+	
+	SELECT FirstName, LastName, City, Address
+	FROM customers
+	WHERE FirstName LIKE '___';
+	
+	/* customers tablosunda, soyisimleri Sch veya Go ile başlayan müşterilerin FirstName,
+	LastName ve City bilgilerini listeleyen sorguyu yazınız*/
+	
+	SELECT FirstName, LastName, City
+	FROM customers
+	WHERE LastName LIKE 'Sch%' OR LastName LIKE 'Go%';
+	
 	
 			
 	
