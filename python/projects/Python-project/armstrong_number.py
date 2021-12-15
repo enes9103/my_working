8110d11d55551eece3b48890c82fef248e910705
EnesUnlu <enes9103@gmail.com>
2021-12-15 13:14:09 +0300

    güncelleme

--- python/projects/Python-project/armstrong_number.py
+++
@@ -1,9 +0,0 @@
-sayi=int(input("bir sayı giriniz:"))
-a=str(sayi)
-liste=[]
-for i in range(0,len(a)):
-    liste.append(int(a[i])**len(a))
-if sum(liste)==int(sayi):
-    print(a + " armstrong sayıdır")
-else:
-    print(a + " armstrong sayı değildir")
